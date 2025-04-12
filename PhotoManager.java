f8d0a42dc8fad7f4b3a19e93bb89bb328a9aaef8
falwa-alkhalifah <162884868+falwa-alkhalifah@users.noreply.github.com>
2025-04-12 17:40:59 +0300

    add Album and PhotoManager classes

---
+++ PhotoManager.java
@@ -0,0 +1,15 @@
+
+package com.mycompany.datastructure;
+
+public class PhotoManager {
+   
+    // Constructor
+    public PhotoManager();
+    // Return all managed photos
+    public LinkedList<Photo> getPhotos();
+    // Add a photo
+    public void addPhoto(Photo p);
+    // Delete a photo
+    public void deletePhoto(String path);
+
+}
