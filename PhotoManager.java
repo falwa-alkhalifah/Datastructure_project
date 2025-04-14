bd7cef095bdd270d1d4a06e1f47dc34b16f156cf
falwa-alkhalifah <162884868+falwa-alkhalifah@users.noreply.github.com>
2025-04-14 05:57:34 +0300

    Update PhotoManager.java add oll the methods

--- PhotoManager.java
+++ PhotoManager.java
@@ -3,13 +3,61 @@ package com.mycompany.datastructure;
 
 public class PhotoManager {
    
-    // Constructor
-    public PhotoManager();
-    // Return all managed photos
-    public LinkedList<Photo> getPhotos();
-    // Add a photo
-    public void addPhoto(Photo p);
-    // Delete a photo
-    public void deletePhoto(String path);
+ LinkedList<Photo> photos;    
 
+    public PhotoManager(){
+        photos = new LinkedList<Photo>();
+    }
+    
+    public LinkedList<Photo> getPhotos(){
+        return photos;
+    }
+    
+    public void addPhoto(Photo p){
+        
+        if(Photo_Exist(photos,p))//to prevent duplicate
+            return;
+        photos.insert(p);
+    }
+    
+    public void deletePhoto(String path){
+        
+        if(photos.empty())
+            return;
+        
+        photos.findfirst();
+        while(!photos.last()){
+            
+            if(photos.retrieve().path.equals(path)){
+                photos.remove();
+                break;
+            }
+
+            photos.findnext();
+        }
+        if(photos.retrieve().path.equals(path))
+               photos.remove();
+        
+    }
+    
+    //check if ther is duplicate photos
+    public boolean Photo_Exist(LinkedList<Photo> L, Photo P){
+        //if the list is empty
+        if(L.empty())
+            return false;
+        //loop through the list
+        L.findfirst();
+        while(!L.last()){
+            
+            if(L.retrieve().path.equals(P.path))
+                return true;
+            L.findnext();
+        }
+        //the last one
+        if(L.retrieve().path.equals(P.path))
+            return true;
+        
+        return false;
+
+    }
 }
