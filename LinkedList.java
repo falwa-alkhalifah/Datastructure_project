cf605dddfa8f09e1abd20731e8eacbbd380b3c54
walaalhajri88 <walaalhajri88@gmail.com>
2025-04-23 15:34:37 +0300

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -1,96 +1,168 @@
-
-
-  class Node<T> {
-    public T data;
-    public Node<T> next;
-
-    public Node (T val) {
-    data = val;
-    next = null;
-    }
- }
-
+/**
+ * Custom implementation of LinkedList data structure
+ * @param <T> Generic type parameter
+ */
 public class LinkedList<T> {
     
-    private Node<T> head;
-    private Node<T> current;
-    
-    public LinkedList () {
-        head = current = null;
+    // Node class for LinkedList
+    private class Node {
+        private T data;
+        private Node next;
+        
+        public Node(T data) {
+            this.data = data;
+            this.next = null;
+        }
     }
     
-    public boolean empty () {
-        return head == null;
+    private Node head;
+    private int size;
+    
+    /**
+     * Constructor for LinkedList
+     */
+    public LinkedList() {
+        this.head = null;
+        this.size = 0;
     }
     
-    public boolean last () {
-        return current.next == null;
+    /**
+     * Insert an element at the end of the list
+     * @param data Element to be inserted
+     */
+    public void insert(T data) {
+        Node newNode = new Node(data);
+        
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        size++;
     }
     
-    public boolean full () {
+    /**
+     * Remove an element from the list
+     * @param data Element to be removed
+     * @return true if element was found and removed, false otherwise
+     */
+    public boolean remove(T data) {
+        if (head == null) {
+            return false;
+        }
+        
+        if (head.data.equals(data)) {
+            head = head.next;
+            size--;
+            return true;
+        }
+        
+        Node current = head;
+        while (current.next != null && !current.next.data.equals(data)) {
+            current = current.next;
+        }
+        
+        if (current.next != null) {
+            current.next = current.next.next;
+            size--;
+            return true;
+        }
+        
         return false;
     }
-     
-    public void findfirst () {
-        current = head;
+    
+    /**
+     * Check if the list contains a specific element
+     * @param data Element to search for
+     * @return true if element is found, false otherwise
+     */
+    public boolean contains(T data) {
+        Node current = head;
+        while (current != null) {
+            if (current.data.equals(data)) {
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
     }
     
-    public void findnext () {
-        current = current.next;
+    /**
+     * Get the size of the list
+     * @return Number of elements in the list
+     */
+    public int size() {
+        return size;
     }
     
-    public T retrieve () {
+    /**
+     * Check if the list is empty
+     * @return true if list is empty, false otherwise
+     */
+    public boolean isEmpty() {
+        return size == 0;
+    }
+    
+    /**
+     * Get an array representation of the list
+     * @return Array containing all elements in the list
+     */
+    @SuppressWarnings("unchecked")
+    public T[] toArray() {
+        T[] array = (T[]) new Object[size];
+        Node current = head;
+        int index = 0;
+        
+        while (current != null) {
+            array[index++] = current.data;
+            current = current.next;
+        }
+        
+        return array;
+    }
+    
+    /**
+     * Get element at specified index
+     * @param index Index of the element to retrieve
+     * @return Element at the specified index
+     * @throws IndexOutOfBoundsException if index is out of range
+     */
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        
+        Node current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        
         return current.data;
     }
     
-    public void update (T val) {
-        current.data = val;
-    }
-    
-    public void insert (T val) {
-        Node<T> tmp;
+    /**
+     * String representation of the list
+     * @return String containing all elements in the list
+     */
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
         
-        if (empty()) {
-            current = head = new Node<T> (val);
-        }
-        else {
-            tmp = current.next;
-            current.next = new Node<T> (val);
+        Node current = head;
+        while (current != null) {
+            sb.append(current.data);
+            if (current.next != null) {
+                sb.append(", ");
+            }
             current = current.next;
-            current.next = tmp;
-   }}
-    
-    public void remove () {
-        
-        if (current == head) {
-            head = head.next;
         }
         
-        else {
-            Node<T> tmp = head;
-            while (tmp.next != current)
-            tmp = tmp.next;
-            tmp.next = current.next;
-        }
-        
-        if (current.next == null)
-            current = head;
-        else
-            current = current.next;
+        sb.append("]");
+        return sb.toString();
     }
-    
-    public void display(){
-        
-        if(head==null)
-            System.out.println("Empty List");
-        Node<T> p = head;
-        
-        while(p != null){
-            System.out.print(p.data + " ");
-            p = p.next;        
-        }
-    }
-
-
 }
-    
