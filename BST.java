1ee8eaaccaf2cff79f1caa775fb35b36c29dfc53
walaalhajri88 <walaalhajri88@gmail.com>
2025-04-23 15:32:31 +0300

    Update BST.java

--- BST.java
+++ BST.java
@@ -1,155 +1,231 @@
-
-    class BSTNode <T> {
-    public String key;
-    public T data;
-    public BSTNode<T> left, right;
-
-    public BSTNode(String k, T val) {
-        key = k;
-        data = val;
-        left = right = null;
+/**
+ * Binary Search Tree implementation
+ * @param <T> Generic type parameter that extends Comparable
+ */
+public class BST<T extends Comparable<T>> {
+    
+    // Node class for BST
+    private class Node {
+        private T data;
+        private Node left;
+        private Node right;
+        
+        public Node(T data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
     }
-
-    public BSTNode(String k, T val, BSTNode<T> l, BSTNode<T> r) {
-        key = k;
-        data = val;
-        left = l;
-        right = r;
-    }
- }
-
-public class BST <T> {
-    BSTNode<T> root, current;
-
+    
+    private Node root;
+    private int size;
+    
+    /**
+     * Constructor for BST
+     */
     public BST() {
-        root = current = null;
-    }
-    public boolean empty() {
-        return root == null;
-    }
-    public boolean full() {
-        return false;
-    }
-    public T retrieve () {
-        return current.data;
-    }
-    public boolean findkey(String k) {
-    BSTNode<T> p = root , q = root;
-    
-    if(empty())
-        return false;
-    
-    while(p != null) {
-        q = p;
-        if(k.compareTo(p.key) == 0) {
-            current = p;
-            return true;
-        }
-        else if(k.compareTo(p.key) < 0)
-            p = p.left;
-        else
-            p = p.right;
-    }
-    current = q;
-    return false;
+        this.root = null;
+        this.size = 0;
     }
     
-    public boolean insert(String k, T val) {
-        BSTNode<T> p, q = current;
-      
-        if(findkey(k)) {
-            current = q;  
-            return false; 
+    /**
+     * Insert an element into the BST
+     * @param data Element to be inserted
+     */
+    public void insert(T data) {
+        root = insertRec(root, data);
+        size++;
+    }
+    
+    /**
+     * Helper method for recursive insertion
+     * @param root Current root node
+     * @param data Element to be inserted
+     * @return Updated root node
+     */
+    private Node insertRec(Node root, T data) {
+        if (root == null) {
+            return new Node(data);
         }
         
-        p = new BSTNode<T>(k, val);
-        if (empty()) {
-            root = current = p;
-            return true;
-        }
-        else {
-            if (k.compareTo(current.key) < 0)
-                current.left = p;
-            else
-                current.right = p;
-            current = p;
-            return true;
+        int compareResult = data.compareTo(root.data);
+        
+        if (compareResult < 0) {
+            root.left = insertRec(root.left, data);
+        } else if (compareResult > 0) {
+            root.right = insertRec(root.right, data);
         }
         
+        return root;
     }
     
-    public boolean removeKey(String k) {
-        BSTNode<T> p = root;
-        BSTNode<T> q = null; 
-        boolean found = false;
-
-        while ((p != null)&&(!found)) {
-            int res = k.compareTo(p.key);
-            if (res < 0) {
-                q = p;
-                p = p.left;
-            } else if (res > 0) {
-                q = p;
-                p = p.right;
-            } else 
-                found=true;
+    /**
+     * Search for an element in the BST
+     * @param data Element to search for
+     * @return true if element is found, false otherwise
+     */
+    public boolean contains(T data) {
+        return containsRec(root, data);
+    }
+    
+    /**
+     * Helper method for recursive search
+     * @param root Current root node
+     * @param data Element to search for
+     * @return true if element is found, false otherwise
+     */
+    private boolean containsRec(Node root, T data) {
+        if (root == null) {
+            return false;
         }
-         if(found){           
-            if ((p.left != null) && (p.right != null)) { // Case 3: two
-                // children
-                // Search for the min in the right subtree
-                BSTNode<T> min = p.right;
-                q = p;
-                while (min.left != null) {
-                q = min;
-                min = min.left;
-                }
-                p.key = min.key;
-                p.data = min.data;
-                deleteNode(min,q);
+        
+        int compareResult = data.compareTo(root.data);
+        
+        if (compareResult == 0) {
+            return true;
+        } else if (compareResult < 0) {
+            return containsRec(root.left, data);
+        } else {
+            return containsRec(root.right, data);
+        }
+    }
+    
+    /**
+     * Get the value associated with a key in the BST
+     * @param key Key to search for
+     * @return Value associated with the key, or null if key not found
+     */
+    public T get(T key) {
+        return getRec(root, key);
+    }
+    
+    /**
+     * Helper method for recursive get
+     * @param root Current root node
+     * @param key Key to search for
+     * @return Value associated with the key, or null if key not found
+     */
+    private T getRec(Node root, T key) {
+        if (root == null) {
+            return null;
+        }
+        
+        int compareResult = key.compareTo(root.data);
+        
+        if (compareResult == 0) {
+            return root.data;
+        } else if (compareResult < 0) {
+            return getRec(root.left, key);
+        } else {
+            return getRec(root.right, key);
+        }
+    }
+    
+    /**
+     * Remove an element from the BST
+     * @param data Element to be removed
+     * @return true if element was found and removed, false otherwise
+     */
+    public boolean remove(T data) {
+        int originalSize = size;
+        root = removeRec(root, data);
+        return size < originalSize;
+    }
+    
+    /**
+     * Helper method for recursive removal
+     * @param root Current root node
+     * @param data Element to be removed
+     * @return Updated root node
+     */
+    private Node removeRec(Node root, T data) {
+        if (root == null) {
+            return null;
+        }
+        
+        int compareResult = data.compareTo(root.data);
+        
+        if (compareResult < 0) {
+            root.left = removeRec(root.left, data);
+        } else if (compareResult > 0) {
+            root.right = removeRec(root.right, data);
+        } else {
+            // Node with only one child or no child
+            if (root.left == null) {
+                size--;
+                return root.right;
+            } else if (root.right == null) {
+                size--;
+                return root.left;
             }
-            else //case 1 and 2
-                deleteNode(p,q);
-            current = root;
-            return true;
-         }
-         return false; 
-    }
-    
-    private void deleteNode(BSTNode<T> n,BSTNode<T> parent){
-        BSTNode<T> child;
-        if(n.left!=null)
-            child=n.left;
-        else
-            child=n.right;
-
-        if (parent == null) { 
-            root = child;  
-       } else {
-        if (n.key.compareTo(parent.key) < 0)
-                parent.left = child;
-            else 
-                parent.right = child;
+            
+            // Node with two children
+            root.data = findMin(root.right);
+            root.right = removeRec(root.right, root.data);
         }
-       }
-    
-    public void inOrder(){
-        if(root == null)
-            System.out.println("empty");
         
-        inOrder(root);
+        return root;
     }
     
-    public void inOrder(BSTNode<T> p){
-        if(p == null)
-            return;
-        inOrder(p.left);
-        System.out.print(p.key + " ");
-        System.out.println(p.data);
-        inOrder(p.right);
+    /**
+     * Find the minimum value in a subtree
+     * @param node Root of the subtree
+     * @return Minimum value in the subtree
+     */
+    private T findMin(Node node) {
+        T minValue = node.data;
+        while (node.left != null) {
+            minValue = node.left.data;
+            node = node.left;
+        }
+        return minValue;
     }
-
-
+    
+    /**
+     * Get the size of the BST
+     * @return Number of elements in the BST
+     */
+    public int size() {
+        return size;
+    }
+    
+    /**
+     * Check if the BST is empty
+     * @return true if BST is empty, false otherwise
+     */
+    public boolean isEmpty() {
+        return size == 0;
+    }
+    
+    /**
+     * Get an in-order traversal of the BST as a LinkedList
+     * @return LinkedList containing all elements in the BST in sorted order
+     */
+    public LinkedList<T> inOrderTraversal() {
+        LinkedList<T> result = new LinkedList<>();
+        inOrderTraversalRec(root, result);
+        return result;
+    }
+    
+    /**
+     * Helper method for recursive in-order traversal
+     * @param root Current root node
+     * @param result LinkedList to store the traversal result
+     */
+    private void inOrderTraversalRec(Node root, LinkedList<T> result) {
+        if (root != null) {
+            inOrderTraversalRec(root.left, result);
+            result.insert(root.data);
+            inOrderTraversalRec(root.right, result);
+        }
+    }
+    
+    /**
+     * String representation of the BST (in-order traversal)
+     * @return String containing all elements in the BST in sorted order
+     */
+    @Override
+    public String toString() {
+        return inOrderTraversal().toString();
+    }
 }
-
-
