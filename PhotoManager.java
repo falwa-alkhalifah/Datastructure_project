34f7323a5996250325b37aa1f660569668272ed1
walaalhajri88 <walaalhajri88@gmail.com>
2025-04-23 15:33:56 +0300

    Update PhotoManager.java

--- PhotoManager.java
+++ PhotoManager.java
@@ -1,61 +1,100 @@
-
+/**
+ * PhotoManager class manages a collection of photos
+ */
 public class PhotoManager {
-   
- LinkedList<Photo> photos;    
-
-    public PhotoManager(){
-        photos = new LinkedList<Photo>();
+    private LinkedList<Photo> photos;
+    
+    /**
+     * Constructor
+     */
+    public PhotoManager() {
+        this.photos = new LinkedList<>();
     }
     
-    public LinkedList<Photo> getPhotos(){
+    /**
+     * Return all managed photos
+     * @return LinkedList of all photos
+     */
+    public LinkedList<Photo> getPhotos() {
         return photos;
     }
     
-    public void addPhoto(Photo p){
-        
-        if(Photo_Exist(photos,p))//to prevent duplicate
-            return;
+    /**
+     * Add a photo
+     * @param p Photo to be added
+     */
+    public void addPhoto(Photo p) {
         photos.insert(p);
     }
     
-    public void deletePhoto(String path){
-        
-        if(photos.empty())
-            return;
-        
-        photos.findfirst();
-        while(!photos.last()){
-            
-            if(photos.retrieve().path.equals(path)){
-                photos.remove();
+    /**
+     * Delete a photo
+     * @param path Path of the photo to be deleted
+     */
+    public void deletePhoto(String path) {
+        int size = photos.size();
+        for (int i = 0; i < size; i++) {
+            Photo photo = photos.get(i);
+            if (photo.getPath().equals(path)) {
+                photos.remove(photo);
                 break;
             }
-
-            photos.findnext();
         }
-        if(photos.retrieve().path.equals(path))
-               photos.remove();
-        
     }
     
-    //check if ther is duplicate photos
-    public boolean Photo_Exist(LinkedList<Photo> L, Photo P){
-        //if the list is empty
-        if(L.empty())
-            return false;
-        //loop through the list
-        L.findfirst();
-        while(!L.last()){
-            
-            if(L.retrieve().path.equals(P.path))
-                return true;
-            L.findnext();
-        }
-        //the last one
-        if(L.retrieve().path.equals(P.path))
-            return true;
+    /**
+     * Find photos that contain all the specified tags
+     * @param tags List of tags to search for
+     * @return LinkedList of photos that contain all the specified tags
+     * @param nbComps Reference to count the number of tag comparisons
+     */
+    public LinkedList<Photo> findPhotosByTags(LinkedList<String> tags, int[] nbComps) {
+        LinkedList<Photo> result = new LinkedList<>();
         
-        return false;
-
+        // If no tags specified, return all photos
+        if (tags.isEmpty()) {
+            return photos;
+        }
+        
+        // Check each photo for all required tags
+        int photoCount = photos.size();
+        for (int i = 0; i < photoCount; i++) {
+            Photo photo = photos.get(i);
+            boolean containsAllTags = true;
+            
+            // Check if the photo contains all required tags
+            int tagCount = tags.size();
+            for (int j = 0; j < tagCount; j++) {
+                String tag = tags.get(j);
+                nbComps[0]++; // Increment comparison counter
+                
+                if (!photo.hasTag(tag)) {
+                    containsAllTags = false;
+                    break;
+                }
+            }
+            
+            if (containsAllTags) {
+                result.insert(photo);
+            }
+        }
+        
+        return result;
+    }
+    
+    /**
+     * Find a photo by its path
+     * @param path Path of the photo to find
+     * @return The photo if found, null otherwise
+     */
+    public Photo findPhotoByPath(String path) {
+        int size = photos.size();
+        for (int i = 0; i < size; i++) {
+            Photo photo = photos.get(i);
+            if (photo.getPath().equals(path)) {
+                return photo;
+            }
+        }
+        return null;
     }
 }
